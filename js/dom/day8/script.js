8738cd05cc6738c6bef1e16f42a31e55e09491b0
nareshpatil17 <patilnaresh8818@gmail.com>
2026-03-26 10:04:36 +0530

    custom cursor project

---
+++ js/dom/day8/script.js
@@ -0,0 +1,5 @@
+let box = document.querySelector(".box")
+
+box.addEventListener("dblclick",function(dets){
+    console.log(dets)
+})  
---
+++ js/dom/dya9/script.js
@@ -0,0 +1,10 @@
+// custom cursor project 
+
+let main = document.querySelector("main")
+
+let img = document.querySelector("img")
+
+main.addEventListener("mousemove",function(elem){
+    img.style.left = elem.x + 'px'
+    img.style.top = elem.y + 'px'
+})
